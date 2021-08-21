4e0c08871506cd0562ea2281ee8b21d4f562c954
Nehemias-Munoz <nehemiasmarvic.dev@gmail.com>
2021-08-21 17:40:30 -0400

    final appearance

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,38 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.teal.shade400,
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Image(
+                  image: AssetImage('assets/images/dados/dice_1.png'),
+                  width: 150.0,
+                ),
+                Image(
+                  image: AssetImage('assets/images/dados/dice_1.png'),
+                  width: 150.0,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          FloatingActionButton(
+            backgroundColor: Colors.black,
+            onPressed: null,
+            child: Icon(Icons.refresh),
+          )
+        ],
+      ),
+    );
   }
 }
